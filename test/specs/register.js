7e7dcb0d249979c2d95a0b6b22b4c8422a54374a
stozhkairyna <stsirasts@gmail.com>
2020-01-14 20:16:12 -0600

    Initial commit

---
+++ test/specs/register.js
@@ -0,0 +1,18 @@
+//
+
+const assert = require('assert')
+
+describe('Register page', () => {
+    it('should have the right title', () => {
+        browser.url('https://stage.pasv.us/user/register')
+        const title = browser.getTitle()
+        assert.strictEqual(title, 'Progress Monitor')
+    })
+
+    it('should have a correct title', () => {
+        const actualH1text = $('h1').getText();
+        const expectedH1text = 'User Register';
+        assert (actualH1text, expectedH1text);
+    })
+
+});
