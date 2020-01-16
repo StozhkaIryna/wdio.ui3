0679a067f14698f3bfac82c9c42191227c4a48fb
stozhkairyna <stsirasts@gmail.com>
2020-01-16 14:30:59 -0600

    UI test Jan 16

---
+++ test/README.md
@@ -0,0 +1,12 @@
+Написать 
+
+* Requirements testing need to be create. /таблица из лекции Jan 14/
+- `или`
+* User stories /пишутся вместо Requirements testing/они проще и короче//- create for this project
+
+// Question for user story = 'as a user I want to: ... in order to:...'//
+
+https://docs.google.com/spreadsheets/d/133bIiGC9-R8aNFxI_-KWpw0LABr9RKUDeqMP-nOZJ9g/edit?usp=sharing 
+
+Tables converter:
+https://tableconvert.com/ 
--- test/specs/register.js
+++ test/specs/register.js
@@ -8,15 +8,68 @@ describe('Register page', () => {
     });
 
     it('should have the right title', () => {
-        const actualTitle = browser.getTitle()
-        const expectedTitle = 'Progress Monitor';
-        expect(actualTitle).equal(expectedTitle);
-    })
+        const actual = browser.getTitle()
+        const expected = 'Progress Monitor';
+        expect(actual).equal(expected);
+    });
 
     it('should have a correct title', () => {
-        const actualH1text = $('h1').getText();
-        const expectedH1text = 'User Register';
-        expect(actualH1text).equal(expectedH1text);
-    })
+        const actual = $('h1').getText();
+        const expected = 'User Register';
+        expect(actual).equal(expected);
+    });
+
+    it('should have a correct description', () => {
+        const actual = $('p').getText();
+        const expected = 'Profiles with fictitious or dummy data will be deleted.';
+        expect(actual).equal(expected);
+    });
+
+    it('should have a correct button text', () => {
+        const actual = $('form button').getText();
+        const expected = 'Submit';
+        expect(actual).equal(expected);
+    });
+
+    it('should fill up first Name field', () => {
+        const element = $('form input[name="firstName"]');
+        element.setValue('John');
+    });
+
+    it('should fill up last Name field', () => {
+        const element = $('form input[name="lastName"]');
+        element.setValue('Smith');
+    });
+
+    it('should fill up phone num field', () => {
+        const element = $('form input[name="phone"]');
+        element.setValue('14523456');
+    });
+
+    it('should fill up email field', () => {
+        const element = $('form input[name="email"]');
+        element.setValue('abc@gmail.com');
+    });
+
+    it('should fill up password field', () => {
+        const element = $('form input[name="password"]');
+        element.setValue('abc123');
+    });
+
+    it('should fill up about field', () => {
+        const element = $('form textarea[name="about"]');
+        element.setValue('one two three');
+    });
+
+    it('should fill up my goals field', () => {
+        const element = $('form textarea[name="goals"]');
+        element.setValue('one two three');
+    });
+
+    it('should chose from dropdown box English level', () => {
+        const element = $('form select[name="englishLevel"]');
+        element.selectByVisibleText('Zero');
+        browser.pause(2000);
+    });
 
 });
